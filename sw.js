76b06a1829378853fea5bdea21678ef37946dc69
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2025-06-10 09:37:56 +0800

    1.0

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'qr-stream-v1';
+const URLS_TO_CACHE = [
+  '.',
+  'index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
